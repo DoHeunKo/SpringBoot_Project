c60e39bcda0de9cfc7fea0e79f6cfd45bde23e3b
user <user@DESKTOP-2HDFTR9>
2023-04-21 08:55:11 +0900

    로그인,회원가입(ajax)

---
+++ study_project/src/main/java/com/hk/study/controller/UserController.java
@@ -0,0 +1,85 @@
+package com.hk.study.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hk.study.command.LoginCommand;
+import com.hk.study.command.RegistCommand;
+import com.hk.study.service.UserService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
+@Controller
+@RequestMapping(value = "/user")
+public class UserController {
+	
+	@Autowired UserService userService;
+	
+	@GetMapping("/loginForm")
+	public String login(Model model) {
+		model.addAttribute("loginCommand",new LoginCommand());
+		return "thymeleaf/user/loginForm";
+	}
+	
+	@PostMapping("/login")
+	public String login(@Validated LoginCommand loginCommand,
+						BindingResult result,
+						HttpServletRequest request, Model model) {
+		
+		if(result.hasErrors()) {
+			return "thymeleaf/user/loginForm";
+		}
+		
+		return userService.userLogin(loginCommand, request);
+	}
+	
+	@GetMapping(value="/registForm")
+	public String addUserForm(Model model) {
+		System.out.println("회원가입 폼이동");
+		model.addAttribute("registCommand",new RegistCommand());
+		return "thymeleaf/user/registForm";
+	}
+	
+	@PostMapping(value="/regist")
+	public String regist(@Validated RegistCommand registCommand,
+						BindingResult result,
+						Model model) {
+		//유효값 처리
+		if(result.hasErrors()) {
+			return "thymeleaf/user/registForm";
+		}
+		try {
+			boolean isS=userService.userRegist(registCommand);
+			return "redirect:/user/loginForm";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "redirect:/user/registForm";
+		}
+	}
+	@GetMapping(value="/userMainForm")
+	public String userMainForm(Model model) {
+		
+		return "thymeleaf/user/userMainForm";
+	}
+	
+	@GetMapping("/idChk")
+	@ResponseBody
+	public Map<String,String> idChk(String id){
+		System.out.println("아이디 중복 체크");
+		String resultId=userService.idChk(id);
+		Map<String,String> map=new HashMap<>();
+		map.put("id", resultId);
+		return map;
+	}
+}
---
+++ study_project/src/main/java/com/hk/study/dtos/UserDto.java
@@ -0,0 +1,26 @@
+package com.hk.study.dtos;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Alias(value="UserDto")
+public class UserDto {
+	private int user_no;
+	private String user_id;
+	private String user_name;
+	private String user_password;
+	private String user_email;
+	private String user_address;
+	private String user_role;
+	private String user_delflag;
+}
